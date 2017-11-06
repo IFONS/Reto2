ec3838035d0f2ed13f534515323f05e51810fdc0
IFONS <smashpit94@gmail.com>
2017-11-06 09:33:17 +0100

    Add files via upload

---
+++ proyecto2/js/js.js
@@ -0,0 +1,11 @@
+function mostrarLogin()
+{   /*Oculta los divs que no nos interesa mostrar y muestro el que queremos*/
+    $("#login").show();
+    $("#registrar").hide();
+}
+
+function mostrarSingUp()
+{   /*Oculta los divs que no nos interesa mostrar y muestro el que queremos*/
+    $("#registrar").show();
+    $("#login").hide();
+}
