f7e67b66f6730c95f7c680a83e3934b8c769a67e
IFONS <smashpit94@gmail.com>
2017-11-10 13:49:27 +0100

    Add files via upload

--- login2/js/js.js
+++ login2/js/js.js
@@ -23,6 +23,8 @@ function registro()
 {
     //comprobar correo()
     comprobarContraseña();
+    comprobarTelefonoF();
+    comprobarTelefonoM();
 }
 
 function login()
@@ -93,7 +95,7 @@ function comprobarCorreoL()
             $.ajax
             ({
                 type: 'POST',
-                url: 'server.php',
+                url: 'index.html',
                 data: email,
                 success: function (data)
                 {
@@ -103,48 +105,103 @@ function comprobarCorreoL()
             });
         });
 }
+
+function comprobarTelefonoM()
+{
+    var tel2 = $('[name=tel2]');
+    function coincideTelf()
+    {
+        var movil=tel2.val();
+        var reg = /^[0-9]{9}$/;
+
+        if( !movil.match(reg) )
+        {
+            $('#feedMov').html('<div class="alert alert-danger" role="alert"> <strong>Warning!</strong> Movil no valido.</div>');
+        }
+        else
+        {
+            $('#feedMov').html('<div class="alert alert-success" role="alert"><strong>SUCCES!</strong> Movil correcto.</div>');
+        }
+
+    }
+
+    tel2.keyup(function () {
+            coincideTelf();
+        }
+
+    )
+}
+
+function comprobarTelefonoF()
+{
+    var tel1 = $('[name=tel1]');
+    var tel2 = $('[name=tel2]');
+    function coincideTelf()
+    {
+        var fijo=tel1.val();
+        var movil=tel2.val();
+        var reg = /^[0-9]{9}$/;
+
+        if( !fijo.match(reg)  )
+        {
+            $('#feedTel').html('<div class="alert alert-danger" role="alert"> <strong>Warning!</strong> Telefono no valido.</div>');
+        }
+        else
+        {
+            $('#feedTel').html('<div class="alert alert-success" role="alert"><strong>SUCCES!</strong> Telefono correcto.</div>');
+        }
+
+    }
+    tel1.keyup(function () {
+            coincideTelf();
+        }
+
+    )
+    tel2.keyup(function () {
+            coincideTelf();
+        }
+
+    )
+}
+
 function comprobarContraseña()
 {
                var pass1 = $('[name=passwordReg]');
                var pass2 = $('[name=passwordReg2]');
-               var confirmacion = "Las contraseñas si coinciden";
-               var longitud = "La contraseña debe estar formada entre 6-10 carácteres (ambos inclusive)";
-               var negacion = "No coinciden las contraseñas";
-               var vacio = "La contraseña no puede estar vacía";
                //oculto por defecto el elemento span
-               var span = $('<span></span>').insertAfter(pass2);
-               span.hide();
                //función que comprueba las dos contraseñas
                function coincidePassword()
                {
                    var valor1 = pass1.val();
                    var valor2 = pass2.val();
                    //muestro el span
-                   span.show().removeClass();
-                   //condiciones dentro de la función
-                   if(valor1 != valor2)
-                   {
-                       $('#feedContras').html('<div class="alert alert-danger" role="alert"> <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong>Warning!</strong> Better check yourself, you\'re not looking too good.</div>');
-                   }
-                   if(valor1.length==0 || valor1=="")
-                   {
-                       $('#feedContras').html('<div class="alert alert-danger" role="alert"> <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong>Warning!</strong> Better check yourself, you\'re not looking too good.</div>');
-                   }
-                   if(valor1.length<6 || valor1.length>10)
-                   {
-                       $('#feedContras').html('<div class="alert alert-danger" role="alert"> <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong>Warning!</strong> Better check yourself, you\'re not looking too good.</div>');
-                   }
-                   if(valor1.length!=0 && valor1==valor2)
-                   {
 
-                       $('#feedContras').html('<div class="alert alert-success" role="alert"> <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong>SUCCES!</strong> Better check yourself, you\\\'re not looking too good.</div>');
+                   if(valor1 != valor2 )
+                   {
+                       $('#feedContras').html('<div class="alert alert-danger" role="alert"> <strong>Warning!</strong> Las contraseñas no coinciden.</div>');
+                   }
+                   else if(valor1.length<6 || valor1.length>10)
+                   {
+                       $('#feedContras').html('<div class="alert alert-danger" role="alert"><strong>Warning!</strong> Longitud incorrecta, minimo 6, maximo 10.</div>');
+                   }
+                   else
+                   {
+                       if(valor1.length!=0 && valor1==valor2)
+                       {
+
+                           $('#feedContras').html('<div class="alert alert-success" role="alert"><strong>SUCCES!</strong> Las contraseñas coinciden.</div>');
+                       }
                    }
                }
                //ejecuto la función al soltar la tecla
-               pass2.keyup(function()
-               {
-                   coincidePassword();
-               });
+                pass1.keyup(function()
+                {
+                    coincidePassword();
+                });
+                pass2.keyup(function()
+                {
+                    coincidePassword();
+                });
 
 
 
